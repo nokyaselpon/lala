a8c004e7df8e9d69606dca5b839dfbc94bcecb9e
nokyaselpon <75005487+nokyaselpon@users.noreply.github.com>
2021-03-02 10:31:03 +0800

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -3,4 +3,4 @@ sudo apt-get update && apt-get upgrade -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.6.2/xmrig-6.6.2-linux-x64.tar.gz
 tar -zxf xmrig-6.6.2-linux-x64.tar.gz
 cd xmrig-6.6.2
-./xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u nokyaselpon@gmail.com -p x
+./xmrig -a cryptonight -o stratum+tcp://49.12.80.43:45700 -u nokyaselpon@gmail.com -p x
